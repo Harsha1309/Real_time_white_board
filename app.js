4a4b490c19679a7de53e99e574aa8b736d4125d0
Harsha1309 <harshyadav1393@gmail.com>
2023-12-28 11:46:55 +0530

    backend completed

---
+++ app.js
@@ -0,0 +1,43 @@
+// const { log } = require("console");
+const express=require("express");
+const socket=require("socket.io");
+
+const app=express();
+
+app.use(express.static("front_end"));
+
+let port=process.env.PORT || 3000;
+let server=app.listen(port,()=>{
+    console.log("Listening to the port : " + port);
+})
+
+let io= socket(server);
+
+io.on("connection",(socket)=>{
+    console.log("Made Socket Connection");
+
+    // Recieved data from one computer to server
+    socket.on("beginPath",(data)=>{
+        // Sending the same data to other computer from server
+        io.sockets.emit("beginPath",data);
+    })
+    socket.on("drawStroke",(data)=>{
+        io.sockets.emit("drawStroke",data);
+    })
+    socket.on("undoRedo",(data)=>{
+        io.sockets.emit("undoRedo",data);
+    })
+
+    socket.on("createSticky",(data)=>{
+        io.sockets.emit("createSticky",data);
+    })
+    socket.on("createSticky1",(data)=>{
+        io.sockets.emit("createSticky1",data);
+    })
+    socket.on("textareaContent",(data)=>{
+        io.sockets.emit("updateContent",data);
+    })
+    socket.on("closeNoteAction",(data)=>{
+        io.sockets.emit("closeNoteAction",data);
+    })
+})
